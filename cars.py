9b554882d3192bcc433b5a07123ba0212ab64d6e
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-08 14:01:56 -0400

    create functions to generate cars in cars class

--- cars.py
+++ cars.py
@@ -1,8 +1,35 @@
+import random
 from turtle import Turtle
 
 class Cars(Turtle):
-    def __init__(self):
+    def __init__(self, shape):
         super(). __init__()
-        pass
-       
+        self.hideturtle()
+        self.initial_speed = 7
+        self.enemy_cars = []
+        self.friendly_cars = []
+
+    def create_enemy_cars(self):
+        random_chance = random.randint(1, 12)
+        if random_chance == 1 or random_chance == 7:
+            enemy_car = Turtle()
+            enemy_car.penup()
+            enemy_car.shape("square")
+            enemy_car.color(random.choice(["red", "blue", "green", "yellow", "purple", "orange"]))
+            enemy_car.shapesize(stretch_wid=.75, stretch_len=2)
+            enemy_car.goto(352, random.randint(-150, 150))
+            enemy_car.setheading(180)
+            self.enemy_cars.append(enemy_car)
+
+    def create_friendly_cars(self):
+        random_chance = random.randint(1, 30)
+        if random_chance == 1:
+            friend_car = Turtle()
+            friend_car.penup()
+            friend_car.shape("square")
+            friend_car.color("black")
+            friend_car.shapesize(stretch_wid=.75, stretch_len=1.75)
+            friend_car.goto(352, random.randint(-150, 150))
+            friend_car.setheading(180)
+            self.friendly_cars.append(friend_car)
     
