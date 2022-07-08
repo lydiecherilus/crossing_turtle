bbf8557cf3cfe4d32d271eb2c009d38b5105b5f5
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-07 21:32:56 -0400

    complete scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,20 @@
-from email.policy import default
 from turtle import Turtle
 
 class Scoreboard(Turtle):
     def __init__(self):
         super(). __init__()
-        pass
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-330, 175)
+        self.write(f"Level: {self.level}", font=("Arial", 16, "normal") )
+
+    def next_level(self):
+        self.level += 1
+        self.clear()
+        self.write(f"Level: {self.level}", font=("Arial", 16, "normal") )
+
+    def game_over(self):
+        self.color("red")
+        self.goto(0, 0)
+        self.write("Game Over", font=("Arial", 20, "normal"))
