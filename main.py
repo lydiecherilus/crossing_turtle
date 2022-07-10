716482c0714efb0ad0a2395357e071d1ff0e471c
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-10 14:17:10 -0400

    create functions to move player and start game

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen = Screen()
 screen.bgcolor("white")
 screen.setup(width=700, height=400)
 screen.title("Turtle Crossing")
+screen.tracer(0)
 
 # create player
 turtle_body = Player()
@@ -22,6 +23,24 @@ scoreboard = Scoreboard()
 
 # create thank you note
 thanks = Thanks()
+
+# create event listeners to move player forward and backward
+screen.listen()
+screen.onkey(turtle_body.move_forward, "Up")
+screen.onkey(turtle_body.move_backward, "Down")
+
+
+# start game
+isgame_on = True
+while isgame_on:
+    time.sleep(0.1)
+    screen.update()
+
+    cars.create_enemy_cars()
+    cars.move_enemy_cars()
+
+    cars.create_friendly_cars()
+    cars.move_friendy_cars()
 
 
 screen.exitonclick()
